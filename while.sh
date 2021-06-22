da6ea257ac518e9e1aba5861118e42ca7dd5f3d3
David Luet <luet@princeton.edu>
2021-06-22 14:13:40 -0400

    Adapted for new version of SLURM

--- while.sh
+++ while.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
-#SBATCH -N 1
 #SBATCH -n 5
 #SBATCH -t 0:5:0
 
 while read a b c
 do
-    srun -n 1 ./code.sh $a $b $c > $a-$b-$c.out &
+    srun --exclusive -n 1 ./code.sh $a $b $c > $a-$b-$c.out &
 done < parameters.dat
 
 wait
