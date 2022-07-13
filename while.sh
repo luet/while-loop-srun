c199dc71aee2a5ad062df290886c7154199e2202
David Luet <luet@princeton.edu>
2022-07-13 13:41:19 -0400

    With for loops

--- while.sh
+++ while.sh
@@ -6,9 +6,15 @@
 # This runs 2 jobs that each has 2 MPI tasks on 2 nodes simultaneously
 module load intel/2021.1.2 intel-mpi/intel/2021.1.1
 
-while read i j
+dirs=(1 2 3 4)
+temps=(a b c d)
+
+for i in "${dirs[@]}"
 do
-    srun --exclusive -N 1 -n 2 ./hello $i $j > $i-$j.out &
-done < parameters.dat
+    for j in "${temps[@]}"
+    do
+	srun --exclusive -N 1 -n 2 ./hello $i $j > $i-$j.out &
+    done
+done
 
 wait
